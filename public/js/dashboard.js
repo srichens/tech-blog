4a0e9c8be672b207d6be035d14f77a80a70d3b2a
Sarah Richens <sarahgrichens@gmail.com>
2023-03-17 13:32:29 -0500

    add js for dashboard

---
+++ public/js/dashboard.js
@@ -0,0 +1,47 @@
+const newFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#comment-name').value.trim();
+  // const needed_funding = document.querySelector('#project-funding').value.trim();
+  const content = document.querySelector('#comment-desc').value.trim();
+
+  if (title && content) {
+    const response = await fetch(`/api/comments`, {
+      method: 'POST',
+      body: JSON.stringify({ title, content }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to create comment');
+    }
+  }
+};
+
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/comments/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete comment');
+    }
+  }
+};
+
+document
+  .querySelector('.new-comment-form')
+  .addEventListener('submit', newFormHandler);
+
+document
+  .querySelector('.comment-list')
+  .addEventListener('click', delButtonHandler);
